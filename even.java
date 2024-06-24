1c77dcc7981a279ddea60881dcd4629a624459f8
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-24 10:47:02 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,16 @@
+
+class even
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=100;i++)
+        {
+        if(i%2==0)
+        {
+            System.out.println("number is even"+i);
+        }
+        }
+        
+    }
+}
