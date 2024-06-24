f34e755ff1597e035ce927eff84afee7b446afcb
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-24 11:04:17 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,12 @@
+class natural
+{
+    public static void main(String args[])
+    {
+        int i,sum=0;
+        for(i=0;i<=100;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println("sum of number"+sum);
+    }
+}
