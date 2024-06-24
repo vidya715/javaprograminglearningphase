771a4ff06ec20cad54adb1d7ac19d879737843ca
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-25 04:07:56 +0530

    Create demo.java
    variable length argument

---
+++ demo.java
@@ -0,0 +1,16 @@
+class demo
+{
+    public static void display(String...player)
+    {
+    for(String p:player)
+    {
+        System.out.println(p+",");
+    }
+    }
+    public static void main(String args[])
+{
+    
+    display("meeta","karuna","sakshi");
+    
+}
+    }
