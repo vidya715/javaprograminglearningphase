5c51a4bfbc895320e4112f8495e7cca5bbc374ac
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-24 10:58:02 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+class evenodd
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=100;i++){
+            if(i%2==0)
+            {
+                System.out.println("number is even ="+i);
+            }
+            else{
+               System.out.println("number is odd ="+i); 
+                
+            }
+            
+        }
+        
+        
+    }
+    
+}
