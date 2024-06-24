2dc7f764fbcb07ff45d87a8c8baf2319e9970e4d
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-25 04:24:28 +0530

    Create dem.java
    commanline argument

---
+++ dem.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class demo
+{
+    public static void main(String args[])
+    {
+        int i;
+        System.out.println("absent student are ....");
+            for(i = 0; i < args.length; i++)
+        {
+           System.out.println(args[i]);
+        }
+        
+        
+        
+        
+    }
+    
+}
