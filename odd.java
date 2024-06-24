7092f10b93a80744a4c3d54b827bdd803df72b51
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-06-24 10:53:48 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,14 @@
+class odd
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=100;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println("odd number is"+i);
+            }
+            
+        }
+    }
+}
