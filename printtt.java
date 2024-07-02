7e3386ca3f5c6c2be3109a9d7be500ee8e9f2fc0
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-02 20:16:47 +0530

    Create printtt.java
    Difference between print and println

---
+++ printtt.java
@@ -0,0 +1,13 @@
+
+class printtt{
+    public static void main(String[] args) {
+        String name="aman";
+        String name1="akuu";
+          System.out.println(" difference between print and println \n");
+        System.out.print(name);
+          System.out.print(name1);
+          System.out.println("\n ______________________________");
+          System.out.println(name);
+          System.out.println(name1);
+    }
+}
