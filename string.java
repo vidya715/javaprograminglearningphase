6a157f271649d55f5aa9c98fdb876f7ff7a4c886
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-02 20:32:20 +0530

    Create string.java
    String function ()

---
+++ string.java
@@ -0,0 +1,15 @@
+class string{
+    public static void main(String[] args) {
+        String name="aman";
+           String name1="akku";
+       System.out.println(name.length());
+         System.out.println(name +"and"+ name1);//concant
+         System.out.println(name.charAt(0));//position
+         String name3=name.replace("a","b");// (oldChar ="a","b");
+          System.out.println(name3);
+        System.out.println(name.substring(0, 3)); // SUBSTRING
+
+         
+       
+    }
+}
