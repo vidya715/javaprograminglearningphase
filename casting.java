e5b0cb141f8d54c629abc504fcebe3e2f3547e35
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 11:20:42 +0530

    Create casting.java
    typecasting in java

---
+++ casting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class casting
+{
+    public static void main(String args[]){
+        double price =100.00;
+        double finalprice=price+18;
+        System.out.println(finalprice);//implict typecasting
+        int p=180;
+        int fp=p+(int)18.0;//explict casting
+         System.out.println(fp);
+        
+        
+        
+    }}
