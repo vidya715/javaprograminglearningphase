b59883eb199d847350fd72242dea13a98ba9c14e
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 12:10:25 +0530

    Create maths.java
    math class

---
+++ maths.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class maths{
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+         System.out.println((int)Math.random());
+        System.out.println((int)(Math.random()*100));
+    
+        
+       
+    }
+}
