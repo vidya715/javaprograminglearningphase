1655642154dd6558e2c9882d20eeae9a3787ee57
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 16:58:34 +0530

    conditionbollean.java
    conditional statement run in java

---
+++ conditionbollean.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class conditionbollean {
+    public static void main(String[] args) {
+        boolean isSunet=true;
+        
+        if (isSunet==true)
+        {
+            System.out.println("day");
+        }
+        else
+        {
+        System.out.println("night");
+        }
+    }
+}
+
