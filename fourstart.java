a22056a79a55296e92267985aebd68791793b937
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 15:36:18 +0530

    fourstart.java
    *
    **
    ***
    ****

---
+++ fourstart.java
@@ -0,0 +1,15 @@
+public class fourstar {
+    public static void main(String[] args) {
+        int line = 4;
+        
+        for(int i=0;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+                
+            }
+            System.out.println(); 
+        }
+}}
+ 
