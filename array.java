c053020fe71da43e3697b6fe57801d2edf1ac35a
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 11:04:15 +0530

    array.java
    simple array methods

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class array{
+    public static void main(String[] args) {
+       int[]marks={87,98,90,84};
+
+       System.out.println(marks[0]);
+       System.out.println(marks.length);
+       
+Arrays.sort(marks);
+System.out.println(marks[0]); // Output: [65, 78, 87, 88, 92]
+
+       
+    }
+}
