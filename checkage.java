bbfe26a59202a27b7e8ee559ef52b345d7f92ecf
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 17:03:30 +0530

    Create checkage.java
    conditional operator

---
+++ checkage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class checkage {
+    public static void main(String[] args) {
+       int age=30;
+       if(age>=18){
+           System.out.println(" can vote");       }
+    else{
+           System.out.println(" can't vote");
+       }
+    }
+}
