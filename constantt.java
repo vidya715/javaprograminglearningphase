da4342c12130dc8e123066e4b228f89f4876d842
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-03 11:27:42 +0530

    constantt.java
    constant value in java using  final keywords

---
+++ constantt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class constantt
+{
+    public static void main(String args[]){
+     final double PI=3.14;//constant variable;
+    int r=5;
+    double area=PI*r*r;
+    System.out.println(area);
+    
+    }}
