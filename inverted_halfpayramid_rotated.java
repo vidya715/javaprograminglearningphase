24f66afcf9c8a3b4c0ced6d4c4b1182f8297fc6a
vidya_jagtap <134387017+vidya715@users.noreply.github.com>
2024-07-04 12:26:38 +0530

    inverted_halfpayramid_rotated.java
    inverted_halfpayramid_rotated by 180 deg

---
+++ inverted_halfpayramid_rotated.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class  inverted_halfpayramid_rotated
+{
+    public static void main(String[] args) {
+     for(int i=1;i<=4;i++) 
+     {
+         for(int j=1;j<=4-i;j++)
+         {
+             System.out.print(" ");
+         }
+           
+           for(int j=1;j<=i;j++)
+           {
+             System.out.print("*");   
+           }
+            System.out.println(" ");
+     }
+    }
+}
